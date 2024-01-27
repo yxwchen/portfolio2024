34d35f3bb6d8e331dd80e140978892b84ed5cd77
Yi-Wen Chen <claire1212@gmail.com>
2024-01-27 21:47:23 +0800

    vite build modify

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,17 @@ const path = require('path')
 export default {
   root: path.resolve(__dirname, 'src'),
   build: {
-    outDir: '../dist'
+    outDir: '../dist',
+    assetsInlineLimit: 0,
+    rollupOptions: {
+      input: {
+        main: 'src/js/main.js',
+        map: 'src/js/map.js',
+        project: 'src/project.html',
+        resume: 'src/resume.html',
+        oldProject: 'src/oldProject.html',
+      },
+    },
   },
   server: {
     port: 8080
